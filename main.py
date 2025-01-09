f285b50c92d72d2c2d87b4f44065fad526c9e097
mobin <abdulmobinkarimi451@gmail.com>
2025-01-09 07:33:11 +0330

    use global project settings for running app

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask
 from views import blueprint
 from dotenv import find_dotenv, load_dotenv
 import os
+from settings import FLASK_HOST, FLASK_PORT
 
 # loading environment variables
 load_dotenv(find_dotenv())
@@ -9,7 +10,8 @@ load_dotenv(find_dotenv())
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+
 app.register_blueprint(blueprint)
 
 if __name__ == '__main__':
-    app.run(debug=1, port=5001)
+    app.run(debug=1, host=FLASK_HOST, port=FLASK_PORT)
